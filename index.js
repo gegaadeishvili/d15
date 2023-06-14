0de0825bda72a3d2220ecae350adc67aac4c0839
Your Name <gegaadeishvili926@gmail.com>
2023-06-14 23:16:39 +0400

    wow commit

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ nums.forEach(num => {
 });
 
 console.log(ganNum);
+
+
+
+const name = 'gega'; 
+const lastName = "adeishvili"; 
+const age = 16; 
+
+if (name.length > 20) {
+  console.log("Name exceeds the character limit");
+}
+
+if (lastName.length > 40) {
+  console.log("Last name exceeds the character limit");
+}
+
+if (age > 18) {
+  const fullName = `${name} ${lastName}`;
+  console.log(`Hello, ${fullName}`);
+}
